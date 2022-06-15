dd312159fa5acbcb073db7138d9856f0726b7309
Amir Akhavan <amir.akhavans@gmail.com>
2022-06-15 17:20:19 +0300

    PLACEHOLDER Variables Selected

--- Task2.sh
+++ Task2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 env > env_vars.txt
 jq -Rs '[ split("\n")[] | select(length > 0) 
-          | split(",") | {FrameworkName: .[0], VersionName: .[1]} ]' env_vars.json
+          | split(",") | {FrameworkName: .[0], VersionName: .[1]} ]' env_vars.txt > 1.json
 env | grep PLACEHOLDER > PLACEHOLDER-vars.txt
 git init
 git add .
