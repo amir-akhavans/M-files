1b5077b61b98c1437edd882de62e36a00e8c5739
Amir Akhavan <amir.akhavans@gmail.com>
2022-06-15 17:30:11 +0300

    Variables

--- Task2.sh
+++ Task2.sh
@@ -4,7 +4,7 @@ jq -Rs '[ split("\n")[] | select(length > 0)
           | split(",") | {FrameworkName: .[0]} ]' env_vars.txt > env_vars.json
 env | grep PLACEHOLDER > PLACEHOLDER-vars.txt
 jq -Rs '[ split("\n")[] | select(length > 0) 
-          | split(",") | {FrameworkName: .[0]} ]' env_vars.txt >PLACEHOLDER-vars.json
+          | split(",") | {FrameworkName: .[0]} ]' PLACEHOLDER-vars.txt > PLACEHOLDER-vars.json
 git init
 git add .
 git commit -m "Variables"
