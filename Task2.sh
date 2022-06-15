096bfe9877959c31553881938a675100988930b7
Amir Akhavan <amir.akhavans@gmail.com>
2022-06-15 17:25:40 +0300

    PLACEHOLDER Variables Selected

--- Task2.sh
+++ Task2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 env > env_vars.txt
 jq -Rs '[ split("\n")[] | select(length > 0) 
-          | split(",") | {FrameworkName: .[0], VersionName: .[1]} ]' env_vars.txt > env_vars.json
+          | split(",") | {FrameworkName: .[0]} ]' env_vars.txt > env_vars.json
 env | grep PLACEHOLDER > PLACEHOLDER-vars.txt
 jq -Rs '[ split("\n")[] | select(length > 0) 
           | split(",") | {FrameworkName: .[0], VersionName: .[1]} ]' env_vars.txt >PLACEHOLDER-vars.json
